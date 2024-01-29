7c86fcd34a1461c390a217f3fc0609c2f050e815
FelixNck <felix.nocke@gmail.com>
2024-01-29 11:02:50 +0100

    Projekt in GitHub synchronisiert und Code für Konsolenoverlay hinzugefügt

---
+++ Warenhaltung/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\nHalle                 Porta Westfalica");
+    printf("\nArtikel:              Artikel:");
+    printf("\nBelegung:             Belegung:");
+    printf("\n  20cm: 40 %%            20cm: 5 %%");
+    printf("\n  40cm: 14 %%            40cm: 51 %%");
+    printf("\n  80cm: 79 %%");
+    printf("\n");
+    printf("\nWarenausgang");
+    printf("\nHalle: 5 Artikel      Porta Westfalica: 6 Artikel");
+    printf("\n");
+    printf("\n(1)   Neuen Artikel anlegen");
+    printf("\n(2)   Artikel bearbeiten");
+    printf("\n(3)   Artikel loeschen");
+    printf("\n(4)   Artikel erfassen");
+    printf("\n(5)   Artikel entfernen");
+    printf("\n(6)   Artikel umraeumen");
+    printf("\n(7)   Artikel ansehen");
+    printf("\n(8)   Lagerbestand zufaellig befuellen");
+    printf("\n(9)   Zufaellige Bestellung erzeugen");
+    printf("\n(10)  Manuelle Bestellung erzeugen");
+    printf("\n(11)  Erfasste Bestellungen versenden");
+    printf("\n(12)  Erfasste Bestellungen abbrechen");
+    printf("\n");
+    printf("\nWaehle eine Auswahlmoeglichkeit:");
+    printf("\n");
+
+    return 0;
+}
